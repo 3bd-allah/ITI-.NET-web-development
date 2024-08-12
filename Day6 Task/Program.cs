f542c85b9365b6e6c4b8c419cc579ac6628892eb
3bd-allah <abdullah.nabil5044@gmail.com>
2024-08-12 13:58:54 +0300

    task day 6 & 7

---
+++ Day6 Task/Program.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Net.Http.Headers;
+using System.Numerics;
+
+namespace Day6_Task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Student student = new Student(100, "Abdullah", 23);
+
+            //int i = student;
+            //Console.WriteLine(i); // 100 
+            //Console.WriteLine(student); // [id]- name age
+
+            Console.WriteLine(sizeof(long));
+            Console.WriteLine(sizeof(int));
+
+
+        }
+    }
+    struct Complex
+    {
+        private int _img;
+        public int Real { get; set; }
+        public int Img
+        {
+            set
+            {
+                if(value < 0) 
+                    _img = 0;
+                _img = value;
+            }
+            get
+            {
+                return _img;
+            }
+        }
+
+        public Complex(int real =0, int img=0)
+        {
+            this.Real = real;
+            this.Img = img;
+        }
+
+        public static bool operator > (Complex a, Complex b) => a.Real>b.Real && a.Img > b.Img;
+        public static bool operator < (Complex a, Complex b) => a.Real < b.Real && a.Img < b.Img;
+        public static bool operator == (Complex a, Complex b) => a.Real == b.Real && a.Img == b.Img;
+        public static bool operator != (Complex a, Complex b) => a.Real != b.Real && a.Img != b.Img;
+        public static Complex operator ++(Complex c)
+        {
+            return new Complex(c.Real++, c.Img++);
+        }
+        public static Complex operator -- (Complex c)
+        {
+            return new Complex(c.Real--, c.Img--);
+        }
+        public override string ToString()
+        {
+            return $"{Real} + {Img}i";
+        }
+    }
+
+    class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public Student(int id= 0, string name= "", int age= 18)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.Age = age;
+        }
+
+        public static bool operator > (Student s1, Student s2)
+        {
+            return s1.Age > s2.Age;
+        }
+
+        public static bool operator < (Student s1, Student s2) => s1.Age < s2.Age;
+
+        public static bool operator == (Student s1, Student s2) => s1.Age == s2.Age;
+        public static bool operator != (Student s1, Student s2) => s1.Age != s2.Age;
+
+        public static implicit operator int(Student s) => s.Id;
+        public static explicit operator string(Student s)=> s.Name;
+        public override string ToString()
+        {
+            return $"[{Id}- {Name} {Age} years old]";
+        }
+    }
+
+}
---
+++ Day7 Task/Program.cs
@@ -0,0 +1,78 @@
+﻿namespace Day7_Task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Dictionary<Student, List<Subject>> dictionary = new();
+
+            dictionary.Add(new Student(2030005, "Abdullah Nabil", 23),
+
+                new List<Subject>
+                {
+                    new Subject(12, "C#", 13),
+                    new Subject(100, "SQL", 20),
+                    new Subject(31, "MVC", 30)
+                }
+            );
+
+            dictionary.Add(new Student(123, "Ahmed Nabil", 30),
+
+                new List<Subject>
+                {
+                    new Subject(23, "Java", 24), 
+                    new Subject(43, "Spring Security", 40),
+                    new Subject(42, "AWS", 40)
+                }
+            );
+
+            foreach(var item in dictionary)
+            {
+                Console.WriteLine(item.Key);
+                foreach(var item2 in item.Value)
+                {
+                    Console.WriteLine(item2);
+                }
+                Console.WriteLine("-------------------------");
+            }
+
+        }
+    }
+    class Subject
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Duration { get; set; }
+
+        public Subject(int id = 1, string name="", int duration= 12)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.Duration = duration;
+        }
+
+        public override string ToString()
+        {
+            return $"[{Id}]- {Name} \t ({Duration} hours)";
+        }
+    }
+    class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public Student(int id = 0, string name = "", int age = 18)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.Age = age;
+        }
+
+        public override string ToString()
+        {
+            return $"[{Id}]- {Name} \t\t{Age} years old";
+        }
+    }
+}
