f86bcdf63350906dbb73419dda3f97de3c9b9882
3bd-allah <abdullah.nabil5044@gmail.com>
2024-09-01 16:52:22 +0300

    implement CRUD operations for Poster task

--- PosterTask/PosterTask/Controllers/UserController.cs
+++ PosterTask/PosterTask/Controllers/UserController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using PosterTask.Models;
 
 namespace PosterTask.Controllers
@@ -10,17 +11,59 @@ namespace PosterTask.Controllers
         {
             this.context = _context;
         }
+
+        // Read All
         public IActionResult Index()
         {
             var users = context.Users.ToList();
             return View(users);
         }
 
+        // Read by Id
         public IActionResult Details(int id)
         {
             var user = context.Users.SingleOrDefault(u => u.Id == id);
             if (user == null) return Content("User is not Found");
             else return View(user);
+        }
+
+        // create 
+        public IActionResult AddForm()
+        {
+            return View();
+        }
+
+        public IActionResult AddToDB(User user)
+        {
+            context.Users.Add(user);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        //Delete 
+        public IActionResult DeleteUser(int id)
+        {
+            var userToDelete = context.Users.Where(user => user.Id == id).SingleOrDefault();
+            context.Users.Remove(userToDelete);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+
+        // Update
+        public IActionResult EditForm(int id)
+        {
+            User user = context.Users.SingleOrDefault(user => user.Id == id);
+            return View(user); 
+        }
+        public IActionResult EditAction(User user)
+        {
+            var userToUpdate = context.Users.Where(u => u.Id == user.Id).SingleOrDefault();
+            if(userToUpdate == null) return Content("Invalid User");
+            userToUpdate.Name = user.Name;
+            userToUpdate.Age = user.Age; 
+            context.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
